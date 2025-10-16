f4af69bda3c749e76837f465891f7bbbe3d646fe
Stan-21 <77824280+Stan-21@users.noreply.github.com>
2025-10-16 19:25:35 +0000

    added simple marker drawing (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,46 @@
 import "./style.css";
 
 document.body.innerHTML = `
-  <canvas id = "myCanvas"></canvas>
+  <canvas id = "myCanvas" width = "256" height = "256"></canvas>
 `;
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const myCanvas = document.getElementById("myCanvas")!;
+const context : CanvasRenderingContext2D = (myCanvas as HTMLCanvasElement).getContext("2d")!;
+console.log(context);
+
+myCanvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+myCanvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+myCanvas.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    drawLine(e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(offsetX: number, offsetY: number) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(x, y);
+  context.lineTo(offsetX, offsetY);
+  context.stroke();
+  context.closePath();
+}
